d69bf49a2fe99a0a59fb0d48b4e58ab952e23007
QHuy1910 <huyttqse183533@fpt.edu.vn>
2025-09-11 10:56:43 +0700

    them phan quyen theo role

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -17,4 +17,16 @@ const protect = (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Chưa đăng nhập" });
+    }
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Không có quyền truy cập" });
+    }
+    next();
+  };
+}
+
+module.exports = { protect, authorizeRoles };
